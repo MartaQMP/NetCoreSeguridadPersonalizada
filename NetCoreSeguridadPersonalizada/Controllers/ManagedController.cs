7a3d3c168c2af46f067d9d72de368f3127c5b50b
MartaQMP <martaquirosmp@gmail.com>
2026-03-10 11:46:18 +0100

    Add project files.

---
+++ NetCoreSeguridadPersonalizada/Controllers/ManagedController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(string userName, string password)
+        {
+            if(userName.ToLower() == "admin" && password == "12345")
+            {
+                /* POR MEDIDAS DE SEGURIDAD, SE GENERA UNA COOKIE CIFRADA QUE ES PARA SABER
+                 * SI EL USER SE HA VALIDADO EN ESTE EXPLORADOR O NO */
+                ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                /* UN CLAIM ES INFORMACION DEL USUARIO */
+                Claim claimUserName = new Claim(ClaimTypes.Name, userName);
+                Claim claimRole = new Claim(ClaimTypes.Role, "USUARIO");
+                identity.AddClaim(claimUserName);
+                identity.AddClaim(claimRole);
+                /* CREAMOS UN USUARIO PRINCIPAL CON ESTA IDENTIDAD */
+                ClaimsPrincipal userPrincipal = new ClaimsPrincipal(identity);
+                /* DAMOS DE ALTA AL USER EN EL SISTEMA */
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme, 
+                    userPrincipal, 
+                    new AuthenticationProperties { ExpiresUtc = DateTime.Now.AddMinutes(10)}
+                );
+                return RedirectToAction("Perfil", "Usuario");
+            }
+            else
+            {
+                ViewBag.Mensaje = "Credenciales incorrectas";
+                return View();
+            }
+                return View();
+        }
+    }
+}
---
+++ NetCoreSeguridadPersonalizada/Controllers/UsuarioController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreSeguridadPersonalizada.Filters;
+
+namespace NetCoreSeguridadPersonalizada.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [AuthorizeUsers]
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
